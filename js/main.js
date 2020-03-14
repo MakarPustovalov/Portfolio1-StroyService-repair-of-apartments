9cfa9c844f90b60cc099192f9626f59962ded36f
MakarPustovalov <makar.pht@gmail.com>
2020-03-14 18:11:27 +0300

    Created sticky menu

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 const menu = document.querySelector('.header__menu');
+const links = document.querySelector('.header__links');
 
 const offset = menu.offsetTop;
 
-window.onscro = () => {
+window.onscroll = () => {
   if (window.pageYOffset >= offset) {
     menu.classList.add('header__menu_fixed');
+    links.classList.add('header__links_fixed');
   } else {
     menu.classList.remove('header__menu_fixed');
+    links.classList.remove('header__links_fixed');
   }
 };
 
